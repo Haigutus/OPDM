6295380da9ae9b1c387cc64d486a6b18ff855c4c
kristjan.vilgo <kristjan.vilgo@elering.ee>
2021-12-17 06:06:58 +0200

    Added getting settings from ENV

--- examples/settings.py
+++ examples/settings.py
@@ -1,15 +1,16 @@
 # Settings
 import os
 
-OPMD_USERNAME = "user"
-OPDM_PASSWORD = "pass"
-OPDM_SERVER = "https://opde.elering.sise:8443"
+OPMD_USERNAME = os.environ.get('OPMD_USERNAME', "user")
+OPDM_PASSWORD = os.environ.get('OPDM_PASSWORD', "pass")
+OPDM_SERVER = os.environ.get('OPDM_SERVER', "https://opde.elering.sise:8443")
 
 # Write here path to specific folder, for now it will write file to the same location from where the script is ran
-EXPORT_FOLDER   = os.getcwd()
+EXPORT_FOLDER = os.environ.get('EXPORT_FOLDER', os.getcwd())
 
-# Put here a path to the floder that contains IGM-s you want to upload
-IGM_SOURCE_FOLDER = r"C:\IGM_EXPORT_PATH"
+# Put here a path to the folder that contains IGM-s you want to upload
+IGM_SOURCE_FOLDER = os.environ.get('IGM_SOURCE_FOLDER', r"C:\IGM_EXPORT_PATH")
 
 # OPDM Client-s that are responsible for providing Ruleset Library (RSL),
-RSL_OFFICIAL_NODES = ["10V1001C--002430", "10V1001C--002422"]
+RSL_OFFICIAL_NODES = os.environ.get('RSL_OFFICIAL_NODES', "10V1001C--002430,10V1001C--002422").split(",")
+
