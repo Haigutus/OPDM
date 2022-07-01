bcab4f45386170aff65f37dc50a711b91a96e8be
Kristjan <kristjan.vilgo@elering.ee>
2022-07-01 07:38:56 +0300

    Fixed __init__.py import order, as version is now used by API it needs to be created in init before import of the module

--- OPDM/__init__.py
+++ OPDM/__init__.py
@@ -1,4 +1,5 @@
-from OPDM.OPDM_SOAP_API import create_client
 from ._version import get_versions
 __version__ = get_versions()['version']
 del get_versions
+from OPDM.OPDM_SOAP_API import create_client
+
